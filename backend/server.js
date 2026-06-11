7cbaea9b97a1510afadf19ed90305b58dd850359
Eshmal Arshad <eshmalsyeda@gmail.com>
2026-06-11 21:50:50 +0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,14 +10,28 @@ app.use(cors({
   origin: 'http://localhost:5173',
 }));
 
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 60, // limit each IP to 60 requests per window
+  message: 'Too many requests from this IP, please try again later.'
+});
+app.use('/api/fetch', limiter);
+
+
+
 // Generic endpoint for multiple APIs
 app.get('/api/fetch', async (req, res) => {
+  const response = await (await import('node-fetch'));
   const { api } = req.query;
 
   const apiUrls = {
     currency: `https://v6.exchangerate-api.com/v6/${process.env.CURRENCY_API_KEY}/latest/USD`,
     omdb: `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.OMDB_API_KEY}`,
     jsonplaceholder: 'https://jsonplaceholder.typicode.com/posts',
+    holiday: `https://holidayapi.com/v1/holidays?key=${process.env.HOLIDAY_API_KEY}&country=US&year=2025&pretty=true`,
+    weather: `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Seattle?unitGroup=metric&key=${process.env.VISUAL_API_KEY}&include=current`,
+    randomuser: 'https://randomuser.me/api/',
+    cocktail: 'https://thecocktaildb.com/api/json/v1/1/search.php?f=a',
   };
 
   if (!apiUrls[api]) return res.status(400).json({ error: 'API not found' });
@@ -42,6 +57,24 @@ app.get('/api/fetch', async (req, res) => {
 
     if (api === 'jsonplaceholder') {
 
+      return res.json(data);
+    }
+    if (api === 'holiday') {
+      // return only the holidays array
+      return res.json(data.holidays);
+    }
+
+    if (api === 'weather') {
+      return res.json({
+        location: data.resolvedAddress,
+        current: data.currentConditions,
+        forecast: data.days,
+      });
+    }
+    if (api === 'randomuser') {
+      return res.json(data.results[0]); // full JSON
+    }
+    if (api === 'cocktail') {
       return res.json(data);
     }
 
